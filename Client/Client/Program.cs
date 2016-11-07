200c69f48dc74cf106b8f6a7ccee0162fed85ff6
Nicholas Chai (Reynolds) <nick@nickchai.com>
2016-11-07 16:41:57 +0800

    Initial commit, console application with client and server communicating to the server and server broadcasting message to all other clients. Next I will need ot see if the server can broadcast objects.

---
+++ Client/Client/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Lidgren.Network;
+
+namespace Client
+{
+    class Program
+    {
+        private static NetClient s_client;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("chat");// define the config object
+            config.AutoFlushSendQueue = false;// set a config setting
+            s_client = new NetClient(config);// create the client object and set it's config 
+
+            // I don't know whata scynchronization context is but aparently this is what i need to do if running from console
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+
+            // define what to do with messages from the server
+            s_client.RegisterReceivedCallback(new SendOrPostCallback(GotMessage));// run this when the client gets a message from the server
+
+            // define the host details
+            string host = "localhost";
+            int port = 14242;
+            // immediatly connect to the server on start up
+            s_client.Start(); //start the client object running 
+            NetOutgoingMessage hail = s_client.CreateMessage("Hi I'd like to connect to you please sir");
+            s_client.Connect(host, port, hail);
+
+
+            Thread.Sleep(1); // give it a second to connect before starting the input loop
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+
+                NetOutgoingMessage om = s_client.CreateMessage(input);
+                s_client.SendMessage(om, NetDeliveryMethod.ReliableOrdered);
+                Console.WriteLine("You: " + input);
+                s_client.FlushSendQueue();
+            }
+
+        }
+
+        public static void GotMessage(object peer)
+        {
+            NetIncomingMessage im;
+            while ((im = s_client.ReadMessage()) != null)
+            {
+                switch (im.MessageType)
+                {
+                    case NetIncomingMessageType.DebugMessage:
+                    case NetIncomingMessageType.ErrorMessage:
+                    case NetIncomingMessageType.WarningMessage:
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                        string text = im.ReadString();
+                        Console.WriteLine(text);
+                        break;
+                    case NetIncomingMessageType.StatusChanged: // when the client connects to the server
+                        NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+                        string reason = im.ReadString();
+                        if (status == NetConnectionStatus.Connected)
+                        {
+                            Console.WriteLine("You have connected to the server!");
+                            Console.WriteLine(status + " : " + reason);
+                        }
+                        break;
+                    case NetIncomingMessageType.Data: // got a message from the server
+                        string message = im.ReadString();
+                        Console.WriteLine(message);
+                        break;
+                    default:
+                        Console.WriteLine("Got some kind of random type: " + im.MessageType);
+                        break;
+                }
+                s_client.Recycle(im);
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Lidgren.Network;
+
+namespace Server
+{
+    class Program
+    {
+        private static NetServer s_server;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("chat");
+            config.MaximumConnections = 100;
+            config.Port = 14242;
+            s_server = new NetServer(config);
+            s_server.Start(); // start the server
+            while (true)
+            {
+                NetIncomingMessage im;
+                // start network server code
+                while ((im = s_server.ReadMessage()) != null)
+                {
+                    switch (im.MessageType)
+                    {
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.VerboseDebugMessage:// if we get some kind of error message print it to the screen
+                            string text = im.ReadString();
+                            Console.WriteLine(text);
+                            break;
+                        case NetIncomingMessageType.StatusChanged:// if the connection status changes, like if a user connects to the server do this. 
+                            NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+                            string reason = im.ReadString();
+                            // print the new users connection ID to the screen
+                            Console.WriteLine(NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " " + status + " " + reason);
+                            if (status == NetConnectionStatus.Connected)
+                            {
+                                Console.WriteLine("Hail: " + im.SenderConnection.RemoteHailMessage.ReadString());
+                            }
+                            break;
+                        case NetIncomingMessageType.Data: // if we get some message from a client
+                            string message = im.ReadString();
+                            Console.WriteLine("Recieved message, Broadcasting: " + message);
+                            List<NetConnection> all = s_server.Connections; // get a copy of all the current connections
+                            all.Remove(im.SenderConnection); // remove the original message sender from the list of connections
+
+                            if (all.Count > 0)// if there is more than 1 user connected to the server send the message, otherwise why bother
+                            {
+                                NetOutgoingMessage outgoing_message = s_server.CreateMessage(); // create out going message object
+                                outgoing_message.Write(NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " said: " + message); // define the message try using outgoing_message.Data to see if we can send objects
+                                s_server.SendMessage(outgoing_message, all, NetDeliveryMethod.ReliableOrdered, 0); // send the message 
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Recieved and unknown type: " + im.MessageType);
+                            break;
+                    }
+                    s_server.Recycle(im); // tells compiler to reuse the message creator pointer so it doesn't need to run the garbage collector
+                }
+                // if the server message ever returns null sleep for 1s and ping again
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
